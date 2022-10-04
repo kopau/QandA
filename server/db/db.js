8ec09f3b0f0ebbd0b4be1d00639c898164b864b4
pko <ko.paul.824@gmail.com>
2022-10-04 10:41:04 -0700

    updated .env

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,22 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+// const pool = new Pool({
+//   host: process.env.HOST,
+//   user: process.env.USER,
+//   port: process.env.DB_PORT,
+//   password: process.env.PASSWORD,
+//   database: process.env.DB_NAME
+// })
+
 const pool = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
   port: process.env.DB_PORT,
-  password: process.env.PASSWORD,
+  password: process.env.PGPASSWORD,
   database: process.env.DB_NAME
 })
+
 
 pool.connect();
 
